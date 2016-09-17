4b2a66afaffd5e285e1e4b4b55d015d085d75b80
Anatoly Borodin <anatoly.borodin@gmail.com>
2016-09-17 13:51:18 +0200

    git: install git/config

--- install.sh
+++ install.sh
@@ -8,9 +8,21 @@ set_dir_vars()
 	readonly DESTDIR="${HOME?}"
 }
 
+install_file()
+{
+	local SRC DST
+	SRC="${REPO?}/${1?}"
+	DST="${DESTDIR?}/${2?}"
+	echo "${SRC} -> ${DST}"
+	mkdir -p "$(dirname "${DST}")"
+	rm -rf "${DST}"
+	cp "${SRC}" "${DST}"
+}
+
 main()
 {
 	set_dir_vars
+	install_file "git/config" ".config/git/config"
 }
 
 main
